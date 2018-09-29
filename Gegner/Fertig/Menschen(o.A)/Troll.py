9c5d8e368623cfc18547187c364c4ac0c505af15
72Joe72 <43684517+72Joe72@users.noreply.github.com>
2018-09-29 15:39:10 +0200

    rename

---
+++ Gegner/Fertig/Menschen(o.A)/Troll.py
@@ -0,0 +1,31 @@
+import random
+
+leben = random.randint(2, 7)
+waffen = random.choice(['Streitkolben'])
+enemy_drop = random.choice(['Streitkolben', '1 Gold', '400 Silber', 'Dietrich', 'Lederbeinschienen', 'Pfeile', 'Nichts', 'Nichts', 'Nichts'])
+geschwindigkeit = random.randint(9, 14)
+staerke = random.randint(5, 7)
+
+print()
+print("Gegner wird erstellt...")
+print()
+print("Troll Stats werden generiert...")
+print()
+print("--Leben:", leben)                                    #Leben
+print()
+print("--Waffe:", waffen)                                   #Waffen-Anzeige
+print()
+print("--Stärke:", staerke)
+print()
+print("--Geschwindigkeit:", geschwindigkeit)  
+print()
+print("--Loot:", enemy_drop)                                #Loot
+print()
+
+    
+    
+
+
+
+
+
